2f30318371296f389d6ac0d973225e0845d4eff5
Ugur Coruh <ugur.coruh.tr@gmail.com>
2021-10-06 00:26:40 +0300

    Sample Project Added

---
+++ Sample Project/Project1/Project1/Main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	int num = 0;
+	printf("Enter Number.\n");
+	scanf("%d", &num);
+	printf("Number is %d.\n",num);
+	return(0);
+}
---
+++ Sample Project/Project1/Project1/build.bat
@@ -0,0 +1,1 @@
+gcc -Wall -O3 Main.c -o Project1
